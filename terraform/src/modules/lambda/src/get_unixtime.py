a025a2582f707f5fda425292ef2151fd0b13fdb9
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2020-07-03 00:04:49 +0900

    [update] lambda

--- terraform/src/modules/lambda/src/get_unixtime.py
+++ terraform/src/modules/lambda/src/get_unixtime.py
@@ -1,10 +1,26 @@
+import base64
+import json
+import time
+
 import os
 import sys
 import pytz
 from datetime import datetime, timedelta
 
 def lambda_handler(event: dict, context):
-    UTC = pytz.timezone('UTC')
-    now = datetime.now(UTC)
-    timestamp = now.timestamp()
-    return timestamp
+    print(datetime.now().isoformat())
+
+    records_count = len(event['Records'])
+    print(f'kinesis recourds count: {records_count}')
+
+    for record in event['Records']:
+        b64_data = record['kinesis']['data']
+        data = base64.b64decode(b64_data).decode('utf-8')
+        print(data)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(
+            {'message': 'hello world'}
+        ),
+    }
