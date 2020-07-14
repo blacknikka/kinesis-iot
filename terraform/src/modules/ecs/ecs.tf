4fed81cfcbb66a5e7995bc9d35bb0a298d71dabe
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2020-07-14 23:48:26 +0900

    [update] using fargate

--- terraform/src/modules/ecs/ecs.tf
+++ terraform/src/modules/ecs/ecs.tf
@@ -1,28 +1,28 @@
 resource "aws_ecs_cluster" "main" {
-    name = var.base_name
+  name = var.base_name
 }
 
 data "template_file" "task_definition" {
   template = file("${path.module}/grafana-task.json")
 
   vars = {
-    image_url = "grafana/grafana:latest"
-    name      = "grafana"
-    log_group_name   = aws_cloudwatch_log_group.app.name
+    image_url      = "grafana/grafana:latest"
+    name           = "grafana"
+    log_group_name = aws_cloudwatch_log_group.app.name
   }
 }
 
 resource "aws_ecs_task_definition" "grafana_task" {
-    family = "grafana_task"
-    network_mode = "awsvpc"
-    cpu = 256
-    memory = 512
-    container_definitions = data.template_file.task_definition.rendered
-    execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  family                = "grafana_task"
+  network_mode          = "awsvpc"
+  cpu                   = 256
+  memory                = 512
+  container_definitions = data.template_file.task_definition.rendered
+  execution_role_arn    = aws_iam_role.ecs_execution_role.arn
 
-    requires_compatibilities = [
-        "FARGATE",
-    ]
+  requires_compatibilities = [
+    "FARGATE",
+  ]
 }
 
 resource "aws_ecs_service" "grafana" {
@@ -31,10 +31,8 @@ resource "aws_ecs_service" "grafana" {
   task_definition = aws_ecs_task_definition.grafana_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-  iam_role        = aws_iam_role.ecs_service_role.arn
-  depends_on      = [
-      aws_iam_role_policy.ecs_service_policy,
-      aws_alb_listener.front_end,
+  depends_on = [
+    aws_alb_listener.front_end,
   ]
 
   load_balancer {
@@ -44,15 +42,15 @@ resource "aws_ecs_service" "grafana" {
   }
 
   network_configuration {
-    subnets         = [
-        var.subnet_for_app.id,
-        var.subnet_for_app2.id,
+    subnets = [
+      var.subnet_for_app.id,
+      var.subnet_for_app2.id,
     ]
 
     security_groups = [
-        aws_security_group.load_balancers.id,
+      aws_security_group.load_balancers.id,
     ]
 
-    assign_public_ip = false
+    assign_public_ip = true
   }
 }
