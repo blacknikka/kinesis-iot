da0c583191463bf7266ec42be667dc41883dda3f
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2020-07-25 17:28:33 +0900

    [add] influxdb

--- terraform/src/modules/ecs/lb.tf
+++ terraform/src/modules/ecs/lb.tf
@@ -1,3 +1,6 @@
+# ----------------------
+# grafana
+# ----------------------
 resource "aws_alb" "app" {
   name            = "elb-for-app"
   security_groups = [aws_security_group.load_balancers.id]
@@ -15,7 +18,7 @@ resource "aws_alb_target_group" "alb_target" {
   vpc_id   = var.vpc_main.id
 }
 
-resource "aws_alb_listener" "front_end" {
+resource "aws_alb_listener" "influx_lb" {
   load_balancer_arn = aws_alb.app.id
   port              = "80"
   protocol          = "HTTP"
@@ -28,6 +31,58 @@ resource "aws_alb_listener" "front_end" {
 
 resource "aws_security_group" "load_balancers" {
   name        = "load_balancers"
+  description = "Allows all traffic"
+  vpc_id      = var.vpc_main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# ----------------------
+# influx
+# ----------------------
+resource "aws_alb" "influx" {
+  name            = "elb-for-influx"
+  security_groups = [aws_security_group.load_balancers_influx.id]
+  subnets         = [
+      var.subnet_for_app.id,
+      var.subnet_for_app2.id,
+  ]
+}
+
+resource "aws_alb_target_group" "alb_target_influx" {
+  name     = "alb-target-for-influx"
+  port     = 8086
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = var.vpc_main.id
+}
+
+resource "aws_alb_listener" "influx_listner_lb" {
+  load_balancer_arn = aws_alb.influx.id
+  port              = 8086
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.alb_target_influx.id
+    type             = "forward"
+  }
+}
+
+resource "aws_security_group" "load_balancers_influx" {
+  name        = "load_balancers_for_influx"
   description = "Allows all traffic"
   vpc_id      = var.vpc_main.id
 
