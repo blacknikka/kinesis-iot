288de5d8f2be1fb9448f49a66f02b7f7b4f80274
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2020-09-13 20:40:41 +0900

    update tag

--- terraform/src/main.tf
+++ terraform/src/main.tf
@@ -86,7 +86,7 @@ data "template_file" "bff_task_definition" {
   template = file("${path.module}/bff-task.json")
 
   vars = {
-    image_url      = module.bff_ecr.ecr_repo.repository_url
+    image_url      = "${module.bff_ecr.ecr_repo.repository_url}:latest"
     name           = "bff"
     region         = data.aws_region.current.name
     log_group_name = aws_cloudwatch_log_group.influx.name
